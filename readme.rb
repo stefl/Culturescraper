0470ae5dcf8b0cb4af4c63e766f067b576155340
steflewandowski <stef@stef.io>
2011-01-28 07:24:36 +0000

    add a readme

---
+++ readme.rb
@@ -0,0 +1,7 @@
+You're using Culturegrid and would like to display a large image of one of the results. Oh - it only supplies a thumbnail?
+
+No fear - run the URL of the resulting web page through this scraper and it will try its best to give you the image on the page most likely to be the image you want.
+
+It's not perfect, but it should improve over time. 
+
+Try it here: http://culturescraper.heroku.com
